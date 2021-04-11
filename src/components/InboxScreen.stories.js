eb86a3f968afa425e6fc8214597afda24cc6e90d
coppe <coppepam@gmail.com>
2021-04-12 02:49:36 +0900

    Modified stories of InboxScreen for setting store to TaskList container

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {action} from '@storybook/addon-actions';
 import {PureInboxScreen} from './InboxScreen';
+import * as TaskListStories from './TaskList.stories';
 
-export default {
-  component: PureInboxScreen,
-  title    : 'InboxScreen',
+// A super-simple mock of a redux store
+const store = {
+  getState: () => {
+    return {
+      tasks: TaskListStories.Default.args.tasks,
+    };
+  },
+  subscribe: () => 0,
+  dispatch : action('dispatch'),
 };
 
-const Template = args => <PureInboxScreen {...args} />
+export default {
+  component : PureInboxScreen,
+  decorators: [story => <Provider store={store}>{story()}</Provider>],
+  title     : 'InboxScreen',
+};
+
+const Template = args => <PureInboxScreen {...args} />;
 
 export const Default = Template.bind({});
 
